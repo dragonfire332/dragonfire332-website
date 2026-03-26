73a60869475cd5fe131ae2b99b7cfe4629f4868c
DragonFire332 <peraltagabriel1604@gmail.com>
2026-03-25 19:36:34 -0600

    <Update Password For Secret Page>

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,8 @@ form.addEventListener("submit", function (e) {
   e.preventDefault(); // prevent page reload
   const pwd = document.getElementById("password").value; // get input value
 
-  if (pwd === "17") {
-    window.location.href = "page4(locked).html"; // redirect if correct
+  if (pwd === "dragonfireisthegoat") {
+    window.location.href = "vX74vfI90oLpErEjkLmN3B4Qq8.html"; // redirect if correct
   } else {
     alert("Incorrect password. Please try again."); // show error
   }
